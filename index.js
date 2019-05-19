ef95150180134f8b2080878c74a1606aea96f213
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-19 11:40:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function logWhispher () {
 function sayHiToGrandma (greeting) {
   if(greeting === "hello") {
     return "I can't hear you!"
+  } else if (greeting === "I love you, Grandma.") {
+    return "I love you, too."
   } else {
     return "YES INDEED!"
   }
